0df7b6419d2aef289ed7d696d6afd63e107f0633
igor atakhanov <igoratakhanov@gmail.com>
2017-12-17 20:44:43 -0500

    Seperated list and seach into their own components.
    Need: fix the bug where closed component breaks input, page 77.

--- src/App.js
+++ src/App.js
@@ -52,17 +52,45 @@ class App extends Component {
     this.setState({ list: updatedList })
   }
   render() {
+    // Destructuring the state object into const list =this.state.list, searchTerm =this.state.searchTerm
+    const {searchTerm, list} = this.state
     return (
       <div className="App">
-        <form>
-        {/*THe function passed to onChange is defined in the component*/}
-          <input
-            type="text"
-            onChange={this.onChangeSearch}
-          />
-        </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item=>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list = {list}
+          pattern = {searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
 
+class Search extends Component{
+  render(){
+    const { value, onChange } = this.props
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          />
+      </form>
+    )
+  }
+}
+
+class Table extends Component{
+  render() {
+    const {list, pattern, onDismiss} = this.props
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -71,22 +99,18 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-            {/* Remember this is basically a for loop, 'for item in..'
-             We require there to be a function in the handler, because
-             otherwise the expression would execute when browser loads page*/}
               <button
-                onClick={()=> console.log(this)}
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
-              > Delete
+              >
+              Dismiss
               </button>
             </span>
           </div>
-
         )}
-
       </div>
-    );
+    )
+
   }
 }
-
 export default App;
