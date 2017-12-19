745c42c24dab42c572b280bb260fbaaffacf9a9b
unknown <igoratakhanov@gmail.com>
2017-12-19 16:50:50 -0500

    Fixed css, page 97

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
             value={searchTerm}
             onChange={this.onChangeSearch}
           >
-          {/* Searchies is child of seach, and in Search Components
+          {/* Searchies is child of search, and in Search Components
             It is referenced as children in props. */}
             Searchies
           </Search>
@@ -90,15 +90,16 @@ const Search = ({value, onChange, children})=>
 
 const Table =({list, pattern, onDismiss})=>
   <div className="table">
+    {/*These are not uls with lis, they are divs with spans per info*/}
     {list.filter(isSearched(pattern)).map(item =>
       <div key={item.objectID} className="table-row">
-        <span>
+        <span style={{ width: '30%'}}>
           <a href={item.url}>{item.title}: </a>
         </span>
-        <span>{item.author} </span>
-        <span>{item.num_comments} </span>
-        <span>{item.points} </span>
-        <span>
+        <span style={{ width:'30%' }}>{item.author} </span>
+        <span style={{ width: '10%' }}>{item.num_comments} </span>
+        <span style={{ width: '10%' }}>{item.points} </span>
+        <span style={{ width: '10%' }}>
           <Button
             onClick={() => onDismiss(item.objectID)}
             className="button-inline"
